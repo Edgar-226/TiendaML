f95e592513afa74c95913b6f077c4e2b2a3d50d1
Edgar Bastida <edgar.bast.95@gmail.com>
2021-08-27 14:09:20 -0500

    Consultas

--- mercadoLibre.js
+++ mercadoLibre.js
@@ -1,17 +1,29 @@
+//Busueda General
+//https://api.mercadolibre.com/sites/MLM/search?q=Motorola%20G6
+//Busqueda por categoria
+//https://api.mercadolibre.com/sites/MLM/search?category=MLM1039
+//Busqueda por id
+//
+
 async function getMercadoLibre() {
 
   let url = "https://api.mercadolibre.com/sites/MLM/search?category=MLM1039";
   let resp = await fetch(url);
 
   const data = await resp.json();
-  console.log(data['results']);
+  console.log(data['results'][0]);
+
+  
+
+
+
 
   let producto = `
     <div class="card" style="width: 18rem;">
-      <img src="https://tiendadml.herokuapp.com/getting-started/logo-developers.png" class="card-img-top"
+      <img src=${data['results'][0]['thumbnail']} class="card-img-top"
       alt="...">
       <div class="card-body">
-        <h5 class="card-title">${data[0]['title']}</h5>
+        <h5 class="card-title">${data['results'][0]['title']}</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         <a href="#" class="btn btn-primary">Go somewhere</a>
       </div>
