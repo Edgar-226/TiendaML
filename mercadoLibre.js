99463e5d462cb19ac6a513262d1188a2bacd17ee
Edgar Bastida <edgar.bast.95@gmail.com>
2021-08-27 15:25:11 -0500

    Consultas Funcionando

--- mercadoLibre.js
+++ mercadoLibre.js
@@ -3,38 +3,68 @@
 //Busqueda por categoria
 //https://api.mercadolibre.com/sites/MLM/search?category=MLM1039
 //Busqueda por id
-//
+//https://api.mercadolibre.com/products/MLA10025564
 
-async function getMercadoLibre() {
-
-  let url = "https://api.mercadolibre.com/sites/MLM/search?category=MLM1039";
+async function mostrarEnPagina (idProducto){
+  let url = 'https://api.mercadolibre.com/items/' + idProducto;
+  console.log(url);
   let resp = await fetch(url);
 
   const data = await resp.json();
-  console.log(data['results'][0]);
-
-  
-
-
+  console.log(data['pictures'][0]['url']);
 
 
   let producto = `
-    <div class="card" style="width: 18rem;">
-      <img src=${data['results'][0]['thumbnail']} class="card-img-top"
-      alt="...">
-      <div class="card-body">
-        <h5 class="card-title">${data['results'][0]['title']}</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>`
+  <div class="card" style="width: 18rem;">
+    <img src=${data['pictures'][0]['url']} class="card-img-top"
+    alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${data['title']}</h5>
+      <p class="card-text">Precio $${data['price']}</p>
+      <a href="#" class="btn btn-primary">Comprar</a>
+      <a href="#" class="btn btn-primary">Agregar al Carrito</a>
+    </div>
+  </div>`
 
 
-  let productoHTML = document.createElement('div');
-  productoHTML.innerHTML += producto;
-  document.getElementById('caja').appendChild(productoHTML);
+let productoHTML = document.createElement('div');
+
+productoHTML.innerHTML += producto;
+document.getElementById('caja').appendChild(productoHTML);
 
 }
+
+
+async function getMercadoLibre() {
+
+  let url = "https://api.mercadolibre.com/sites/MLM/search?category=MLM1039&offset=10&limit=10";
+  let resp = await fetch(url);
+
+  const data = await resp.json();
+  console.log(data['results']);
+
+  console.log(data['results'][0]['id'])
+
+ for (let i = 0; i < data['results'].length; i++) {
+  mostrarEnPagina(data['results'][i]['id'])
+   
+ }
+
+  // mostrarEnPagina(data['results'][0]['id'])
+  // mostrarEnPagina(data['results'][1]['id'])
+  // mostrarEnPagina(data['results'][2]['id'])
+  // mostrarEnPagina(data['results'][3]['id'])
+ 
+
+}
+
+
+
+
+
+
+
+
 
 getMercadoLibre()
 
