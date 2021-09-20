1ec58af2ec98ba4191bb50f942c4027269a9b21e
Edgar Bastida <edgar.bast.95@gmail.com>
2021-09-20 16:00:05 -0500

    Conexion de front con api db products

--- front/scripts/mercadoLibre.js
+++ front/scripts/mercadoLibre.js
@@ -21,6 +21,30 @@ async function mostrarEnPagina(idProducto, contenedor = 'cajaML') {
   }
 
 }
+async function mostrarEnPaginaAnyEd(idProducto, contenedor = 'caja') {
+  if (document.getElementById(contenedor)) {
+    let url = 'http://localhost:3000/products/' + idProducto;
+    let resp = await fetch(url);
+    const data = await resp.json();
+    console.log(await data[0][0]['id'])
+    let producto = `
+  <div class="border contenido-producto" style="justify-content: center;">
+    <img src=${data[0][0]['picture']} class="card-img-top"
+    alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${data[0][0]['name']}</h5>
+      <p class="card-text">$${data[0][0]['price'].toFixed(2)}</p>
+      
+      <button class="btn btn-primary"  onclick="agregarProducto('${data[0][0]['id']}')">Agregar al Carrito</button>
+    </div>
+  </div>`
+    let productoHTML = document.createElement('div');
+    productoHTML.classList.add('contenedor-producto', 'col-xl-3', 'col-md-4', 'col-sm-6', 'p-3', 'Secondary')
+    productoHTML.innerHTML += producto;
+    document.getElementById(contenedor).appendChild(productoHTML);
+  }
+
+}
 
 async function getMercadoLibre() {
   let url = 'http://localhost:3000/ml';
@@ -41,11 +65,24 @@ async function buscarMercadoLibre(busqueda, contenedor) {
     mostrarEnPagina(data['results'][i]['id'], contenedor)
   }
 }
+async function buscarAnYEd() {
+  let url = 'http://localhost:3000/products';
+  let resp = await fetch(url);
+  const data = await resp.json();
+  //console.log(data[0][0]);
+  for (let i = 0; i < data[0].length; i++) {
+    mostrarEnPaginaAnyEd(data[0][i]['id'])
+  }
+}
+
+
+
 
 buscarMercadoLibre('polaroid', 'cajaPolaroid')
 buscarMercadoLibre('kodak', 'cajaKodak')
 buscarMercadoLibre('nikon', 'cajaNikon')
 getMercadoLibre()
+buscarAnYEd()
 
 function irCarrito() {
   window.location.href = "./carrito.html";
